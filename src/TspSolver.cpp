8c69163a8ec32e1115c33fb5b81d6ceed6b27f81
a <a@Mac.lan>
2026-07-16 10:22:38 +0800

    Keep full 1-tree benchmarks isolated

--- src/TspSolver.cpp
+++ src/TspSolver.cpp
@@ -1134,7 +1134,9 @@ BranchBoundSolver::OneTree BranchBoundSolver::computeOneTree(
     result.feasible = true;
     result.cost = cost;
     result.edges = std::move(edges);
+#ifndef TSP_DISABLE_INCREMENTAL_ONETREE
     initializeDynamicMst(result);
+#endif
     return result;
 }
 
